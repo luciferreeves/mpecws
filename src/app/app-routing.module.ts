0a339176a9e66e6a66635861d6decbb0bb8feca0
Kumar Priyansh <bobbyskhs@gmail.com>
2020-05-23 23:07:40 +0530

    Initialised main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: MainPageComponent },
 ];
 
 @NgModule({
