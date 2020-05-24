846c3700bb96552f774ce9c9fd3410bc48a770a4
Kumar Priyansh <bobbyskhs@gmail.com>
2020-05-24 22:18:22 +0530

    Main page completed design

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-main-page',
@@ -9,7 +10,43 @@ export class MainPageComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    const visibilityPreferences = localStorage.getItem('visibilityPreferences');
+    if (!visibilityPreferences) {
+      localStorage.setItem('visibilityPreferences', 'light');
+      this.setTheme();
+    } else {
+      this.setTheme();
+    }
+
+  }
+
+  setTheme() {
+    const visibilityPreferences = localStorage.getItem('visibilityPreferences');
+    if (visibilityPreferences === 'dark') {
+      $('.themeableComponent').addClass('inverted');
+      $('.themedComponent').addClass('dark');
+    } else {
+      $('.themeableComponent').removeClass('inverted');
+      $('.themedComponent').removeClass('dark');
+    }
+  }
+
+  toggleDarkTheme() {
+    const visibilityPreferences = localStorage.getItem('visibilityPreferences');
+    if (visibilityPreferences === 'dark') {
+      localStorage.setItem('visibilityPreferences', 'light');
+      this.setTheme();
+    } else {
+      localStorage.setItem('visibilityPreferences', 'dark');
+      this.setTheme();
+    }
+  }
+
+  toggleSidebar() {
+    $('.mobileMenu').toggle();
+    $('body, html').toggleClass('noscroll');
   }
 
 }
